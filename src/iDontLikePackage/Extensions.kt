542c2723721628ed7d5b6e01358319fb84f8fb22
shandyCho <whqjarn111@gmail.com>
2026-02-03 17:58:08 +0900

    확장함수 생성까지 공부함

---
+++ src/iDontLikePackage/Extensions.kt
@@ -0,0 +1,8 @@
+package com.example.iDontLikePackage
+
+// 확장함수
+fun IDontLikePackageSystem.extension() {
+    println("this is extension")
+    // 확장대상 클래스명.확장함수명 의 형태로 지정이 가능하다
+    // 확장함수를 사용할 곳, 확장함수의 대상이 되는 확장 대상 클래스와 다른 패키지에 있어도 됨
+}
--- src/kotlinPractice/KotlinIntermediate.kt
+++ src/kotlinPractice/KotlinIntermediate.kt
@@ -1,5 +1,26 @@
 package com.example.kotlinPractice
 
+import com.example.iDontLikePackage.IDontLikePackageSystem
+import com.example.iDontLikePackage.extension
+
+
 fun intermediate() {
+    // 확장함수
+    // 확장함수를 추가해서 클래스를 확장할 수 잇음
+    // 확장함수는 클래스의 멤버 함수를 호출하는 것과 동일한 방식으로 호출할 수 있음
+    // 리시버
+    // 리시버는 함수가 호출되는 대상으로, 정보가 공유되는 대상 또는 주체로 볼 수 있음
+
+    val readOnlyShape = listOf<String>("One", "Two", "Three")
+    println("readOnlyShape.first(): ${readOnlyShape.first()}")
+    // readOnlyShape.first(): One
+    // first() 함수를 호출하는 것은 intermediate() 이지만,
+    // first() 함수는 readOnlyShape에 속해있음
+    // 따라서 readOnlyShape의 first()가 호출이 되는 것으로, 함수가 호출이 되는 대상은 readOnlyShape임
+    // 즉 first()의 리시버는 readOnlyShape가 됨
+
+    val ins = IDontLikePackageSystem("s", 1, false)
+    ins.extension()
+    // this is extension
 
 }
